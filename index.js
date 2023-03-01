6281b2e2a119ed0b45cc5516e7f8dd9a31ef8c3b
hellokallyne <hellookallyne@gmail.com>
2023-03-01 12:36:34 -0300

    Add getComputerChoice function

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const score = {
   player: 0,
   computer: 0,
 };
+
+function getComputerChoice() {
+  const randomNumberBetween0And2 = Math.floor(
+    Math.random() * possibleChoices.length
+  );
+  const computerChoice = possibleChoices[randomNumberBetween0And2];
+
+  return computerChoice;
+}
